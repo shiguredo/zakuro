5b56fe015f8769a5e68213986d5056dd2b80a56b
melpon <shigemasa7watanabe+github@gmail.com>
2023-04-04 09:40:00 +0900

    Sora C++ SDK の feature/gen-client に対応

--- src/virtual_client.cpp
+++ src/virtual_client.cpp
@@ -21,8 +21,85 @@
 #include "nop_video_decoder.h"
 #include "sctp_transport_factory.h"
 
-VirtualClient::VirtualClient(VirtualClientConfig config)
-    : sora::SoraDefaultClient(config_), config_(config) {}
+std::shared_ptr<VirtualClient> VirtualClient::Create(
+    VirtualClientConfig config) {
+  auto vc = std::make_shared<VirtualClient>();
+  vc->config_ = std::move(config);
+
+  sora::SoraClientContextConfig context_config;
+  context_config.use_hardware_encoder = false;
+  context_config.use_audio_device = false;
+
+  context_config.configure_media_dependencies =
+      [vc](const webrtc::PeerConnectionFactoryDependencies& dependencies,
+           cricket::MediaEngineDependencies& media_dependencies) {
+        media_dependencies.adm = dependencies.worker_thread->BlockingCall([&] {
+          ZakuroAudioDeviceModuleConfig admconfig;
+          admconfig.task_queue_factory = dependencies.task_queue_factory.get();
+          if (vc->config_.audio_type ==
+              VirtualClientConfig::AudioType::Device) {
+#if defined(__linux__)
+            webrtc::AudioDeviceModule::AudioLayer audio_layer =
+                webrtc::AudioDeviceModule::kLinuxAlsaAudio;
+#else
+            webrtc::AudioDeviceModule::AudioLayer audio_layer =
+                webrtc::AudioDeviceModule::kPlatformDefaultAudio;
+#endif
+            admconfig.type = ZakuroAudioDeviceModuleConfig::Type::ADM;
+            admconfig.adm = webrtc::AudioDeviceModule::Create(
+                audio_layer, dependencies.task_queue_factory.get());
+          } else if (vc->config_.audio_type ==
+                     VirtualClientConfig::AudioType::NoAudio) {
+            webrtc::AudioDeviceModule::AudioLayer audio_layer =
+                webrtc::AudioDeviceModule::kDummyAudio;
+            admconfig.type = ZakuroAudioDeviceModuleConfig::Type::ADM;
+            admconfig.adm = webrtc::AudioDeviceModule::Create(
+                audio_layer, dependencies.task_queue_factory.get());
+          } else if (vc->config_.audio_type ==
+                     VirtualClientConfig::AudioType::SpecifiedFakeAudio) {
+            admconfig.type = ZakuroAudioDeviceModuleConfig::Type::FakeAudio;
+            admconfig.fake_audio = vc->config_.fake_audio;
+          } else if (vc->config_.audio_type ==
+                     VirtualClientConfig::AudioType::AutoGenerateFakeAudio) {
+            admconfig.type = ZakuroAudioDeviceModuleConfig::Type::Safari;
+          } else if (vc->config_.audio_type ==
+                     VirtualClientConfig::AudioType::External) {
+            admconfig.type = ZakuroAudioDeviceModuleConfig::Type::External;
+            admconfig.render = vc->config_.render_audio;
+            admconfig.sample_rate = vc->config_.sample_rate;
+            admconfig.channels = vc->config_.channels;
+          }
+          return ZakuroAudioDeviceModule::Create(std::move(admconfig));
+        });
+
+        auto sw_config = sora::GetSoftwareOnlyVideoEncoderFactoryConfig();
+        sw_config.use_simulcast_adapter = true;
+        sw_config.encoders.push_back(sora::VideoEncoderConfig(
+            webrtc::kVideoCodecH264,
+            [openh264 = vc->config_.openh264](
+                auto format) -> std::unique_ptr<webrtc::VideoEncoder> {
+              return webrtc::DynamicH264VideoEncoder::Create(
+                  cricket::VideoCodec(format), openh264);
+            }));
+        media_dependencies.video_encoder_factory =
+            absl::make_unique<sora::SoraVideoEncoderFactory>(
+                std::move(sw_config));
+        media_dependencies.video_decoder_factory.reset(
+            new NopVideoDecoderFactory());
+      };
+
+  context_config.configure_dependencies =
+      [vc](webrtc::PeerConnectionFactoryDependencies& dependencies) {
+        dependencies.call_factory = CreateFakeNetworkCallFactory(
+            vc->config_.fake_network_send, vc->config_.fake_network_receive);
+        dependencies.sctp_factory.reset(
+            new SctpTransportFactory(dependencies.network_thread));
+      };
+
+  vc->context_ = sora::SoraClientContext::Create(context_config);
+
+  return vc;
+}
 
 void VirtualClient::Connect() {
   if (closing_) {
@@ -47,13 +124,14 @@ void VirtualClient::Connect() {
     if (config_.disable_highpass_filter)
       ao.highpass_filter = false;
     std::string audio_track_id = rtc::CreateRandomString(16);
-    audio_track_ = factory()->CreateAudioTrack(
-        audio_track_id, factory()->CreateAudioSource(ao).get());
+    audio_track_ = context_->peer_connection_factory()->CreateAudioTrack(
+        audio_track_id,
+        context_->peer_connection_factory()->CreateAudioSource(ao).get());
   }
   if (!config_.no_video_device) {
     std::string video_track_id = rtc::CreateRandomString(16);
-    video_track_ =
-        factory()->CreateVideoTrack(video_track_id, config_.capturer.get());
+    video_track_ = context_->peer_connection_factory()->CreateVideoTrack(
+        video_track_id, config_.capturer.get());
 
     if (config_.fixed_resolution) {
       video_track_->set_content_hint(
@@ -62,12 +140,14 @@ void VirtualClient::Connect() {
   }
 
   sora::SoraSignalingConfig config = config_.sora_config;
-  config.pc_factory = factory();
+  config.pc_factory = context_->peer_connection_factory();
   config.observer = shared_from_this();
-  config.network_manager = signaling_thread()->BlockingCall(
-      [this]() { return connection_context()->default_network_manager(); });
-  config.socket_factory = signaling_thread()->BlockingCall(
-      [this]() { return connection_context()->default_socket_factory(); });
+  config.network_manager = context_->signaling_thread()->BlockingCall([this]() {
+    return context_->connection_context()->default_network_manager();
+  });
+  config.socket_factory = context_->signaling_thread()->BlockingCall([this]() {
+    return context_->connection_context()->default_socket_factory();
+  });
 
   signaling_ = sora::SoraSignaling::Create(config);
   signaling_->Connect();
@@ -106,76 +186,6 @@ VirtualClientStats VirtualClient::GetStats() const {
   st.datachannel_connected = signaling_->IsConnectedDataChannel();
   st.websocket_connected = signaling_->IsConnectedWebsocket();
   return st;
-}
-
-void VirtualClient::ConfigureDependencies(
-    webrtc::PeerConnectionFactoryDependencies& dependencies) {
-  cricket::MediaEngineDependencies media_dependencies;
-
-  media_dependencies.task_queue_factory = dependencies.task_queue_factory.get();
-  media_dependencies.adm = worker_thread()->BlockingCall([&] {
-    ZakuroAudioDeviceModuleConfig admconfig;
-    admconfig.task_queue_factory = dependencies.task_queue_factory.get();
-    if (config_.audio_type == VirtualClientConfig::AudioType::Device) {
-#if defined(__linux__)
-      webrtc::AudioDeviceModule::AudioLayer audio_layer =
-          webrtc::AudioDeviceModule::kLinuxAlsaAudio;
-#else
-      webrtc::AudioDeviceModule::AudioLayer audio_layer =
-          webrtc::AudioDeviceModule::kPlatformDefaultAudio;
-#endif
-      admconfig.type = ZakuroAudioDeviceModuleConfig::Type::ADM;
-      admconfig.adm = webrtc::AudioDeviceModule::Create(
-          audio_layer, dependencies.task_queue_factory.get());
-    } else if (config_.audio_type == VirtualClientConfig::AudioType::NoAudio) {
-      webrtc::AudioDeviceModule::AudioLayer audio_layer =
-          webrtc::AudioDeviceModule::kDummyAudio;
-      admconfig.type = ZakuroAudioDeviceModuleConfig::Type::ADM;
-      admconfig.adm = webrtc::AudioDeviceModule::Create(
-          audio_layer, dependencies.task_queue_factory.get());
-    } else if (config_.audio_type ==
-               VirtualClientConfig::AudioType::SpecifiedFakeAudio) {
-      admconfig.type = ZakuroAudioDeviceModuleConfig::Type::FakeAudio;
-      admconfig.fake_audio = config_.fake_audio;
-    } else if (config_.audio_type ==
-               VirtualClientConfig::AudioType::AutoGenerateFakeAudio) {
-      admconfig.type = ZakuroAudioDeviceModuleConfig::Type::Safari;
-    } else if (config_.audio_type == VirtualClientConfig::AudioType::External) {
-      admconfig.type = ZakuroAudioDeviceModuleConfig::Type::External;
-      admconfig.render = config_.render_audio;
-      admconfig.sample_rate = config_.sample_rate;
-      admconfig.channels = config_.channels;
-    }
-    return ZakuroAudioDeviceModule::Create(std::move(admconfig));
-  });
-  media_dependencies.audio_encoder_factory =
-      webrtc::CreateBuiltinAudioEncoderFactory();
-  media_dependencies.audio_decoder_factory =
-      webrtc::CreateBuiltinAudioDecoderFactory();
-
-  auto sw_config = sora::GetSoftwareOnlyVideoEncoderFactoryConfig();
-  sw_config.use_simulcast_adapter = true;
-  sw_config.encoders.push_back(sora::VideoEncoderConfig(
-      webrtc::kVideoCodecH264,
-      [openh264 = config_.openh264](
-          auto format) -> std::unique_ptr<webrtc::VideoEncoder> {
-        return webrtc::DynamicH264VideoEncoder::Create(
-            cricket::VideoCodec(format), openh264);
-      }));
-  media_dependencies.video_encoder_factory =
-      absl::make_unique<sora::SoraVideoEncoderFactory>(std::move(sw_config));
-  media_dependencies.video_decoder_factory.reset(new NopVideoDecoderFactory());
-
-  media_dependencies.audio_mixer = nullptr;
-  media_dependencies.audio_processing =
-      webrtc::AudioProcessingBuilder().Create();
-
-  dependencies.media_engine =
-      cricket::CreateMediaEngine(std::move(media_dependencies));
-
-  dependencies.call_factory = CreateFakeNetworkCallFactory(
-      config_.fake_network_send, config_.fake_network_receive);
-  dependencies.sctp_factory.reset(new SctpTransportFactory(network_thread()));
 }
 
 void VirtualClient::OnSetOffer(std::string offer) {
